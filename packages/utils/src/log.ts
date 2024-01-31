7b3078fd757f87f777cc0f5f33690dcaeea32cfb
Andrew Schoen <schoen.andrewj@gmail.com>
2024-01-31 08:55:20 -0800

    Added a leva control for Chris

--- packages/utils/src/log.ts
+++ packages/utils/src/log.ts
@@ -1,3 +1,5 @@
-export function log(value: string) {
+export function log(value: string): void {
+  /* eslint-disable no-console -- this specifically tests logging */
   console.log(value);
+  /* eslint-enable no-console -- turn back on after test */
 }
