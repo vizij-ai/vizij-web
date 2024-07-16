2320fa4f92a157fbee17a10deeaff79eddaeba71
Andrew Schoen <schoen.andrewj@gmail.com>
2024-07-16 13:36:24 -0700

    interpolation and linting fixes

--- packages/utils/src/json.ts
+++ packages/utils/src/json.ts
@@ -1,29 +1,28 @@
-
-
-function replacer(key: string, value: any) {
-    if(value instanceof Map) {
-      return {
-        dataType: 'Map',
-        value: Array.from(value.entries()), // or with spread: value: [...value]
-      };
-    } else {
-      return value;
-    }
-}
-
-function reviver(key: string, value: any) {
-    if(typeof value === 'object' && value !== null) {
-        if (value.dataType === 'Map') {
-        return new Map(value.value);
-        }
-    }
+function replacer(key: string, value: any): any {
+  if (value instanceof Map) {
+    return {
+      dataType: "Map",
+      value: Array.from(value.entries()), // or with spread: value: [...value]
+    };
+  } else {
     return value;
+  }
 }
 
-export function stringifyMapped(value: any): string{
-    return JSON.stringify(value, replacer, 2);
+function reviver(key: string, value: any): any {
+  if (typeof value === "object" && value !== null) {
+    if (value.dataType === "Map") {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument -- safe because we know the type
+      return new Map(value.value);
+    }
+  }
+  return value;
+}
+
+export function stringifyMapped(value: any): string {
+  return JSON.stringify(value, replacer, 2);
 }
 
 export function parseToMapped(json: string): any {
-    return JSON.parse(json, reviver);
+  return JSON.parse(json, reviver);
 }
