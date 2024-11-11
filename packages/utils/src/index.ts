51d49dcd9feefafa5aaf256254a0101fe5473184
Andrew Schoen <schoen.andrewj@gmail.com>
2024-11-11 10:43:31 -0800

    cleaned up imports

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,43 +1,22 @@
-export { log } from "./log";
-export { getNamespace, getLookup, getId } from "./namespace";
-export {
-  type Player,
-  reset,
-  now,
-  update,
-  play,
-  pause,
-  seek,
-  setBounds,
-  setViewport,
-  newPlayer,
-} from "./player";
-export {
-  usePlayerStore,
-  type PlayerData,
-  type PlayerActions,
-  type PlayerStore,
-} from "./player-store";
+export * from "./log";
+export * from "./namespace";
+export * from "./player";
+export * from "./player-store";
 export * from "./animated-values";
-export { numberToDurationString } from "./time";
-export { pairwise } from "./pairwise";
-export { useLazy } from "./lazy";
-export { getHexagonPath, getDegenerateHexagonPath } from "./hexagon";
-export { closestFrame } from "./closest-frame";
-export { toRadians, toDegrees } from "./angles";
-export { alpha, hexToRgba, rgbToRgba, hslToRgba, altColor } from "./colors";
-export { useDeep } from "./use-deep";
-export { useMediaQuery } from "./use-media-query";
-export type { Write, StoreSubscribeWithSelector } from "./zustand";
-export { stringifyMapped, parseToMapped, parseJSONFileEvent } from "./json";
-export { downloadBlob, downloadJSONFile } from "./download";
-export { pointsToPath } from "./points-to-path";
-export { angularDistance, eulerToRotationMatrix, rotationMatrixToAngle } from "./euler";
-export { overlappingSegments } from "./overlapping-segments";
-export {
-  type Progress,
-  incrementProgress,
-  composeProgress,
-  type PendingResultWithProgress,
-} from "./progress";
-export { Result } from "./result";
+export * from "./time";
+export * from "./pairwise";
+export * from "./lazy";
+export * from "./hexagon";
+export * from "./closest-frame";
+export * from "./angles";
+export * from "./colors";
+export * from "./use-deep";
+export * from "./use-media-query";
+export * from "./zustand";
+export * from "./json";
+export * from "./download";
+export * from "./points-to-path";
+export * from "./euler";
+export * from "./overlapping-segments";
+export * from "./progress";
+export * from "./result";
