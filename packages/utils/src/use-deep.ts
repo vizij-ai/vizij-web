cadc737752b73fb2c4cfd4b1392e0bd0b7874e23
Andrew Schoen <schoen.andrewj@gmail.com>
2025-01-21 12:31:38 -0800

    complex queries

--- packages/utils/src/use-deep.ts
+++ packages/utils/src/use-deep.ts
@@ -1,11 +1,20 @@
 import { useRef } from "react";
 import deepEqual from "deep-equal";
 
-export function useDeep<S, U>(selector: (state: S) => U): (state: S) => U {
+export function useDeepSelector<S, U>(selector: (state: S) => U): (state: S) => U {
   const prev = useRef<U>();
 
   return (state) => {
     const next = selector(state);
     return deepEqual(prev.current, next) ? (prev.current as U) : (prev.current = next);
   };
+}
+
+export function useDeep<V, D>(initializer: () => V, dependencies: D[]): V {
+  const prevValue = useRef<V>();
+  const prevDependencies = useRef<D[]>([]);
+
+  return deepEqual(prevDependencies.current, dependencies)
+    ? (prevValue.current as V)
+    : (prevValue.current = initializer());
 }
