fc3115994ef08677664f868530e0947967acf29f
Saad Elbeleidy <sbeleidy90@gmail.com>
2025-05-06 16:29:56 -0400

    feat: move AWS work to firebase function

--- apps/website/functions/main.py
+++ apps/website/functions/main.py
@@ -1,15 +1,68 @@
+import flask
+import boto3
+import os
+import json
 from firebase_admin import initialize_app
 from firebase_functions import https_fn
-import flask
+from dotenv import load_dotenv
+from flask import jsonify, make_response
+
+load_dotenv()
 
 initialize_app()
 app = flask.Flask(__name__)
+polly_client = boto3.Session(aws_access_key_id=os.getenv("AWS_ACCESS_KEY"), aws_secret_access_key=os.getenv("AWS_SECRET_KEY"), region_name="us-east-1").client("polly")
 
 
 @app.get("/")
 def get_home():
-    return {"message": "Welcome to the Vizij API"}
+    return jsonify({"message": "Welcome to the Vizij API"})
 
+@app.post("/tts/get-audio")
+def get_audio():
+    try:
+        data = flask.request.get_json()
+        voice = data["voice"] if "voice" in data else "Ruth"
+
+        audio_response = polly_client.synthesize_speech(VoiceId=voice,
+                OutputFormat='mp3',
+                Text = data["text"],
+                Engine = 'neural')
+
+        audio = audio_response["AudioStream"].read()
+
+        audio_res = make_response(audio)
+        audio_res.headers['Content-Type'] = 'audio/mpeg'
+        print(audio_res)
+
+        return audio_res
+
+
+    except:
+        print("Failed to get response from Polly")
+        return jsonify({
+            "status": "An error occurred while getting the AWS Polly Audio and Visemes"
+        })
+
+@app.post("/tts/get-visemes")
+def get_visemes():
+    try:
+        data = flask.request.get_json()
+        voice = data["voice"] if "voice" in data else "Ruth"
+
+        viseme_response = polly_client.synthesize_speech(VoiceId=voice, OutputFormat= "json",Text = data["text"],SpeechMarkTypes= ["sentence", "word", "viseme"],Engine = 'neural')
+
+        visemes = viseme_response["AudioStream"].read().decode().split("\n")
+        list_of_visemes = [json.loads(v) for v in visemes[:-1] ]
+
+        return jsonify({
+            "visemes": list_of_visemes
+        })
+    except:
+        print("Failed to get response from Polly")
+        return jsonify({
+            "status": "An error occurred while getting the AWS Polly Audio and Visemes"
+        })
 
 @https_fn.on_request()
 def api(req: https_fn.Request) -> https_fn.Response:
