e7cd9fa94629fbb73050a1b2cf8c744cd0842177
Saad Elbeleidy <sbeleidy90@gmail.com>
2025-05-07 15:16:58 -0400

    fix: update vizij types to allow undefined

--- packages/render/src/store-types.ts
+++ packages/render/src/store-types.ts
@@ -22,7 +22,7 @@ export interface VizijActions {
   setValue: (
     id: string,
     namespace: string,
-    value: RawValue | ((current: RawValue | undefined) => RawValue),
+    value: RawValue | ((current: RawValue | undefined) => RawValue | undefined),
   ) => void;
   setWorldElementName: (id: string, value: string) => void;
   setVizij: (scene: World, animatables: Record<string, AnimatableValue>) => void;
