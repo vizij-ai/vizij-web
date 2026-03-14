815d087f9f468fa4c6acefcf29ad46874a1f6cbd
Chris Birmingham <cmbirmingham19@gmail.com>
2026-03-13 18:12:02 -0700

    Revert "Merge remote-tracking branch 'origin/main' into develop"
    This reverts commit 024aacf674c57e51586b0159521ce7673663834d, reversing
    changes made to 6a4a1a275afaf52cf50c33172f4345ba754ea975.

--- apps/vizij-authoring/src/components/scene-composer/index.ts
+++
@@ -1,1 +0,0 @@
-export { SceneComposerWorkbench } from "./SceneComposerWorkbench";
--- apps/vizij-authoring/src/poseRig/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { PoseRigWorkbench } from "./PoseRigWorkbench";
-export { PoseList } from "./PoseList";
-export { PoseEditor } from "./PoseEditor";
-export { NeutralEditor } from "./NeutralEditor";
-export { PoseSummary } from "./PoseSummary";
